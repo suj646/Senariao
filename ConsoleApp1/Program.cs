704052038d7f69015e9d36817954a98c61fc546a
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:13:17 +0530

    Senariao

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program c1 = new Program();
+            c1.sce();
+        }
+
+        public void sce()
+        {
+            Console.WriteLine("Enter your Department");
+            string Department = (Console.ReadLine());
+
+            if (Department.ToUpper() == "IT" || Department.ToUpper() == "CSC")
+            {
+                Console.WriteLine("Enter your Percentage");
+                int Percentage = Convert.ToInt32(Console.ReadLine());
+
+                if (Percentage >= 60)
+                {
+                    Console.WriteLine("Enter your age");
+                    int Age = Convert.ToInt32(Console.ReadLine());
+                    if (Age < 50)
+                    {
+                        Console.WriteLine("You are eligible for this post");
+                    }
+                    else
+                        Console.WriteLine("Your age is not suitable for the requirement");
+                }
+                else
+                    Console.WriteLine("Your Percentage is not suitable for the requirement");
+            }
+            else
+                Console.WriteLine("Your qualification is not suitable for the requirement");
+        }
+
+    }
+}
---
+++ scenariostatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace scenariostatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int marks = 65;
+            if (marks >= 80)
+            {
+                Console.WriteLine("Student has passed with higher first class");
+            }
+            else if (marks >= 60)
+            {
+                Console.WriteLine("Student has passed with first class");
+            }
+            else if (marks >= 40)
+            {
+                Console.WriteLine("Student has passed with second class");
+            }
+            else
+            {
+                Console.WriteLine("Student has failed");
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
